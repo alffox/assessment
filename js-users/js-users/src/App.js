12f4c5d44b0a431a02411d5316c9200c1ac02729
alffox <alfonso.crisci.it@gmail.com>
2020-02-04 19:43:34 +0100

    feat: Add pagination

--- js-users/js-users/src/App.js
+++ js-users/js-users/src/App.js
@@ -2,14 +2,26 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
-import Button from 'react-bootstrap/Button';
+import Pagination from 'react-bootstrap/Pagination' // Inspired by https://react-bootstrap.netlify.com/components/pagination/#pagination
 
 class App extends React.Component {
-  state = {
-    error: null,
-    isLoaded: false,
-    users: []
-  };
+  constructor() {
+    super();
+    this.state = {
+      error: null,
+      isLoaded: false,
+      users: [],
+      currentPage: 1,
+      usersPerPage: 10
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(event) {
+    this.setState({
+      currentPage: Number(event.target.id)
+    });
+  }
 
   componentDidMount() {
     fetch("http://js-assessment-backend.herokuapp.com/users.json")
@@ -34,6 +46,37 @@ class App extends React.Component {
   }
 
   render() {
+    // Solution inspired by https://stackoverflow.com/questions/40232847/how-to-implement-pagination-in-reactjs
+    const indexOfLastUser = this.state.currentPage * this.state.usersPerPage;
+    const indexOfFirstUser = indexOfLastUser - this.state.usersPerPage;
+    const currentUsers = this.state.users.slice(indexOfFirstUser, indexOfLastUser);
+
+    const renderUsers = currentUsers.map((user, index) => {
+      return (
+        <li key={index}>
+          {user.first_name}
+        </li>
+      )
+    });
+
+    const pageNumbers = [];
+    for (let i = 1; i <= Math.ceil(this.state.users.length / this.state.usersPerPage); i++) {
+      pageNumbers.push(i);
+    }
+
+    const renderPageNumbers = pageNumbers.map(number => {
+      return (
+        <Pagination.Item
+          key={number}
+          id={number}
+          active={number === this.state.currentPage}
+          onClick={this.handleClick}
+        >
+          {number}
+        </Pagination.Item>
+      );
+    });
+
     return (
       <div className="App">
         <header className="App-header">
@@ -41,16 +84,14 @@ class App extends React.Component {
           <p>
             Edit <code>src/App.js</code> and save to reload.
         </p>
-          <Button variant="primary">Primary</Button>{' '}
-          <ul>
-            {this.state.users.map((user, index) => {
-              return (
-                <li key={index}>
-                  {user.first_name}
-                </li>
-              )
-            })}
-          </ul>
+          <div>
+            <ul>
+              {renderUsers}
+            </ul>
+            <div>
+              <Pagination size="sm">{renderPageNumbers}</Pagination>
+            </div>
+          </div>
           <a
             className="App-link"
             href="https://reactjs.org"
