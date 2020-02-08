0a65fa9d98be842c16214882381881e092ef67c0
alffox <alfonso.crisci.it@gmail.com>
2020-02-08 11:24:11 +0100

    feat: Add spacing around Form

--- js-users/js-users/src/modules/JSCreateUser.js
+++ js-users/js-users/src/modules/JSCreateUser.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Form, Button } from 'react-bootstrap';
+import { Row, Col, Form, Button } from 'react-bootstrap';
 
 class JSCreateUser extends React.Component {
 
@@ -34,17 +34,23 @@ class JSCreateUser extends React.Component {
 
     render() {
         return (
-            <Form>
-                <Form.Group controlId="first-name">
-                    <Form.Control type="input" placeholder="Enter User's First Name" defaultValue={this.props.new_first_name} onBlur={e => this.handleNewFirstName(e)} />
-                </Form.Group>
-                <Form.Group controlId="last-name">
-                    <Form.Control type="text" placeholder="Enter User's Last Name" defaultValue={this.props.new_last_name} onBlur={e => this.handleNewLastName(e)} />
-                </Form.Group>
-                <Button variant="dark" type="button" onClick={(e) => this.handleNewUserFormSubmission(e)}>
-                    Submit
-            </Button>
-            </Form>
+            <div>
+                <br />
+                <Form>
+
+                    <Form.Group controlId="first-name">
+                        <Form.Control type="input" placeholder="Enter User's First Name" defaultValue={this.props.new_first_name} onBlur={e => this.handleNewFirstName(e)} />
+                    </Form.Group>
+
+                    <Form.Group controlId="last-name">
+                        <Form.Control type="text" placeholder="Enter User's Last Name" defaultValue={this.props.new_last_name} onBlur={e => this.handleNewLastName(e)} />
+                    </Form.Group>
+                    <Button variant="dark" type="button" onClick={(e) => this.handleNewUserFormSubmission(e)}>
+                        Submit
+                    </Button>
+                </Form>
+                <br />
+            </div>
         );
     }
 }
