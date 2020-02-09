df9a3576b45c22fe413d2ffdafed191238fd613d
alffox <alfonso.crisci.it@gmail.com>
2020-02-09 13:08:50 +0100

    bug: Improve spinner visibility

--- js-users/js-users/src/modules/JSUsersPulse.js
+++ js-users/js-users/src/modules/JSUsersPulse.js
@@ -7,7 +7,12 @@ class JSUsersPulse extends React.Component {
     render() {
         return (
             <div className="text-center">
-                <Spinner animation="grow" />Loading data ...
+                <Spinner
+                    animation="border"
+                    variant="light"
+                    className="spinner"
+                />
+                <div className="spinner">Loading data ...</div>
             </div>
         );
     }
