9eba43cdacd699bc88fe475b59dda92cf84027fc
alffox <alfonso.crisci.it@gmail.com>
2020-02-09 09:56:56 +0100

    bug: Removed unused BS components

--- js-users/js-users/src/App.js
+++ js-users/js-users/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 
-import { Container, Row, Nav, Navbar, Image } from 'react-bootstrap';
+import { Container, Nav, Navbar } from 'react-bootstrap';
 
 import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
 
