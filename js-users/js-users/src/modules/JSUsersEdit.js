8941a51e5e1f0e3cc15c8cdfdf4c574f721f8cd3
alffox <alfonso.crisci.it@gmail.com>
2020-02-10 12:25:32 +0100

    chore: Update engines for heroku deployment

---
+++ js-users/js-users/src/modules/JSUsersEdit.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+class JSUsersEdit extends React.Component {
+    render() {
+        return (
+            <h1 className="text-center">ciao</h1>
+        );
+    }
+}
+
+export default JSUsersEdit;
